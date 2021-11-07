e4908466adabd0298cd4535bff75e84b93f3e60a
Simen August Tinderholt <simtind@users.noreply.github.com>
2021-11-07 17:05:29 +0100

    Handle advertising name in setup_startup function.

--- pyedurov2/system/__init__.py
+++ pyedurov2/system/__init__.py
@@ -3,7 +3,7 @@ import shutil
 from pathlib import Path
 import os
     
-def setup_startup():
+def setup_startup(name="edurov"):
     system_directory = Path(__file__).parent
     systemd_user_dir = Path("/lib/systemd/system/")
 
@@ -12,7 +12,7 @@ def setup_startup():
 
     with open(system_directory / "pyedurov2.service") as source:
         with open(systemd_user_dir / "pyedurov2.service", "w") as target:
-            target.write(source.read().format(user_name = user))
+            target.write(source.read().format(user_name = user, advertising_name=name))
 
     os.system("systemctl daemon-reload")
     os.system("systemctl enable pyedurov2.service")
